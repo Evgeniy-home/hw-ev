69010b15a63fcbfce11a6bcf6579bdb0266fb0f3
Evgeniy <62895441+Evgeniy-home@users.noreply.github.com>
2021-09-26 23:43:53 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,54 +2,71 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"strconv"
 	"strings"
-	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
+type state int
+
+const (
+	begin state = iota
+	escape
+	symbol
+)
+
 func Unpack(s string) (string, error) {
-	var builtString strings.Builder
-	var multiplier strings.Builder
-	var previousChar rune
-	var isEscaped bool
+	var prevRune rune
+	state := begin
+
+	b := strings.Builder{}
+
 	for _, r := range s {
-		if r == '\\' && !isEscaped {
-			// если руна равна \\ и не бул
-			isEscaped = true
-			continue
-		}
-		if isEscaped {
-			builtString.WriteRune(r)
-			previousChar = r
-			isEscaped = false
-			continue
-		}
-		// Если r - цифра (0-9)
-		if unicode.IsDigit(r) {
-			// если предыдущая руна больше 0
-			if previousChar > 0 {
-				multiplier.WriteRune(r)
-				// добавляем руну в строку
-			} else {
-				return "", nil
+		switch {
+		case '0' <= r && r <= '9':
+			switch state {
+			case begin:
+				return "", ErrInvalidString
+			case escape:
+				prevRune = r
+				state = symbol
+			case symbol:
+				for i := rune(0); i < r-'0'; i++ {
+					b.WriteRune(prevRune)
+				}
+				state = begin
 			}
-		} else { // если руна не цифра
-			if m, err := strconv.Atoi(multiplier.String()); err == nil && m > 0 && previousChar > 0 { // если строку нельзя конвертировать в инт, это не первый символ и предыдущая руна больше 0
-				builtString.WriteString(strings.Repeat(string(previousChar), m-1)) // строим строку, где повторяем данный символ на один меньше, чем номер
-				multiplier.Reset()                                                 // сбрасываем мультиплайер
+		case r == '\\':
+			switch state {
+			case begin:
+				state = escape
+			case escape:
+				prevRune = r
+				state = symbol
+			case symbol:
+				b.WriteRune(prevRune)
+				state = escape
 			}
-			builtString.WriteRune(r) //записываем руну, если предыдущие условия не сработали
-			previousChar = r
+		default:
+			switch state {
+			case begin:
+				prevRune = r
+				state = symbol
+			case escape:
+				return "", ErrInvalidString
+			case symbol:
+				b.WriteRune(prevRune)
+				prevRune = r
+			}
 		}
 	}
-	if m, err := strconv.Atoi(multiplier.String()); err == nil && m > 0 && previousChar > 0 {
-		builtString.WriteString(strings.Repeat(string(previousChar), m-1))
+
+	switch state {
+	case begin:
+	case escape:
+		return "", ErrInvalidString
+	case symbol:
+		b.WriteRune(prevRune)
 	}
-	if res := builtString.String(); len(res) > 0 {
-		return builtString.String(), nil
-	} else {
-		return "", nil
-	}
+	return b.String(), nil
 }
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,26 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+
+	taskCh := make(chan Task)
+	var wg sync.WaitGroup
+	var errCnt int32
+
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			for task := range taskCh {
+				task()
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		taskCh <- task
+	}
+	close(taskCh)
+
+	wg.Wait()
 	return nil
 }
